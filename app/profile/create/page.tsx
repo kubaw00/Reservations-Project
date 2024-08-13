41c963460fc6fadd7a93906f5f804f24c8d54c19
Jakub Widurek <jakubwidurek@gmail.com>
2024-08-13 23:32:14 +0200

    Add submit button and formcontainer

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -1,6 +1,7 @@
 import { Label } from '@/components/ui/label';
-import { Input } from '@/components/ui/input';
-import { Button } from '@/components/ui/button';
+import FormInput from '@/components/form/FormInput';
+
+import { SubmitButton } from '@/components/form/Buttons';
 
 const createProfileAction = async (formData: FormData) => {
   'use server';
@@ -14,13 +15,13 @@ function CreateProfile() {
       <h1 className='text-2xl font-semibold mb-8 capitalize'>new user</h1>
       <div className='border p-8 rounded-md max-w-lg'>
         <form action={createProfileAction}>
-          <div className='mb-2'>
-            <Label htmlFor='firstName'>First Name</Label>
-            <Input id='firstName' name='firstName' type='text' />
-          </div>
-          <Button type='submit' size='lg'>
-            Create Profile
-          </Button>
+          <FormInput
+            label='First Name'
+            name='name'
+            type='text'
+            placeholder='your first name...'
+          />
+          <SubmitButton text='Create Profile' />
         </form>
       </div>
     </section>
