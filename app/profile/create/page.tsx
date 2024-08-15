4128c9864e537cc05ff60f8247f2f4b598df972c
Jakub Widurek <jakubwidurek@gmail.com>
2024-08-15 13:55:39 +0200

    add Prisma, Zod and create form action with parse method

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -1,28 +1,21 @@
-import { Label } from '@/components/ui/label';
 import FormInput from '@/components/form/FormInput';
-
 import { SubmitButton } from '@/components/form/Buttons';
-
-const createProfileAction = async (formData: FormData) => {
-  'use server';
-  const firstName = formData.get('firstName') as string;
-  console.log(firstName);
-};
+import FormContainer from '@/components/form/FormContainer';
+import { createProfileAction } from '@/utils/actions';
 
 function CreateProfile() {
   return (
     <section>
       <h1 className='text-2xl font-semibold mb-8 capitalize'>new user</h1>
       <div className='border p-8 rounded-md max-w-lg'>
-        <form action={createProfileAction}>
-          <FormInput
-            label='First Name'
-            name='name'
-            type='text'
-            placeholder='your first name...'
-          />
-          <SubmitButton text='Create Profile' />
-        </form>
+        <FormContainer action={createProfileAction}>
+          <div className='grid gap-4 mt-4 '>
+            <FormInput type='text' name='firstName' label='First Name' />
+            <FormInput type='text' name='lastName' label='Last Name' />
+            <FormInput type='text' name='username' label='Username' />
+            <SubmitButton text='Create Profile' className='mt-8' />
+          </div>
+        </FormContainer>
       </div>
     </section>
   );
