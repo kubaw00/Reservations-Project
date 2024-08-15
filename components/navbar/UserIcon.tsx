11cfa01a4eb12f082abb16b32714c7d6ba28a7df
Jakub Widurek <jakubwidurek@gmail.com>
2024-08-15 15:33:51 +0200

    add fetchImageProfile action

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,6 +1,19 @@
 import { LuUser2 } from 'react-icons/lu';
+import { fetchProfileImage } from '@/utils/actions';
 
-function UserIcon() {
+async function UserIcon() {
+  const profileImage = await fetchProfileImage();
+
+  if (profileImage) {
+    return (
+      // eslint-disable-next-line @next/next/no-img-element
+      <img
+        className='w-6 h-6 rounded-full'
+        src={profileImage}
+        alt='Profile image'
+      />
+    );
+  }
   return <LuUser2 className='w-6 h-6 bg-primary rounded-full text-white' />;
 }
 export default UserIcon;
